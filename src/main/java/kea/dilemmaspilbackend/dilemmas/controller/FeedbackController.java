8b20032403ed43801d963534a59753b39bbb50a5
bekjo <albe194e@stud.kea.dk>
2023-05-09 15:22:10 +0200

    fdsf

--- src/main/java/kea/dilemmaspilbackend/dilemmas/controller/FeedbackController.java
+++ src/main/java/kea/dilemmaspilbackend/dilemmas/controller/FeedbackController.java
@@ -3,10 +3,7 @@ package kea.dilemmaspilbackend.dilemmas.controller;
 import kea.dilemmaspilbackend.dilemmas.model.Feedback;
 import kea.dilemmaspilbackend.dilemmas.repository.FeedbackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -16,6 +13,12 @@ public class FeedbackController {
     @Autowired
     FeedbackRepository feedbackRepository;
 
+
+    @GetMapping("/api/get/feedback")
+    public Iterable<Feedback> getFeedback(){
+
+        return feedbackRepository.findAll();
+    }
 
     @PostMapping("/api/post/feedback")
     public void createFeedback(@RequestBody Feedback feedback){
